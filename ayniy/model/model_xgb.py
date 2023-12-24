2da558f144108f1c42d1dce82446407aca112c19
Naoyg <ab96.naoto@akane.waseda.jp>
2023-12-24 04:47:21 +0000

    pathlib xgb

--- ayniy/model/model_xgb.py
+++ ayniy/model/model_xgb.py
@@ -1,10 +1,14 @@
-import os
+from pathlib import Path
 
 import numpy as np
 import pandas as pd
 import xgboost as xgb
 from ayniy.model.model import Model
 from ayniy.utils import Data
+
+HOME_PATH = Path(__file__).resolve().parents[2]
+MODEL_DIR = HOME_PATH / "output/model"
+MODEL_DIR.mkdir(exist_ok=True)
 
 
 class ModelXGB(Model):
@@ -58,11 +62,10 @@ class ModelXGB(Model):
         return fold_importance_df
 
     def save_model(self) -> None:
-        model_path = os.path.join("../output/model", f"{self.run_fold_name}.model")
-        os.makedirs(os.path.dirname(model_path), exist_ok=True)
+        model_path = MODEL_DIR / f"{self.run_fold_name}.model"
         # best_ntree_limitが消えるのを防ぐため、pickleで保存することとした
         Data.dump(self.model, model_path)
 
     def load_model(self) -> None:
-        model_path = os.path.join("../output/model", f"{self.run_fold_name}.model")
+        model_path = MODEL_DIR / f"{self.run_fold_name}.model"
         self.model = Data.load(model_path)
