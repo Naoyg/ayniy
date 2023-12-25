7ba5bcfaafeef23315f531586241701dca68f78d
Naoyg <ab96.naoto@akane.waseda.jp>
2023-12-25 13:57:42 +0000

    eda

--- experiments/eda.py
+++ experiments/eda.py
@@ -25,13 +25,19 @@ def load_dataset(cfg):
     return X_train, X_test
 
 
+def replace_inf(df):
+    to_replace = {np.inf: 9999999999, -np.nan: -9999999999}
+    return df.replace(to_replace)
+
+
 @hydra.main(config_name="config_eda", config_path="conf")
 def main(cfg: DictConfig):
-    save_dir = HOME_PATH / "report"
+    save_dir = HOME_PATH / "output/report"
     save_dir.mkdir(exist_ok=True)
     cwd_hydra = Path.cwd()
 
     df_train, df_test = load_dataset(cfg)
+    df_train, df_test = replace_inf(df_train), replace_inf(df_test)
 
     report = sv.analyze(
         [df_train, "Training Data"], target_feat=cfg.cols_definition.target_col, pairwise_analysis="off")
@@ -42,11 +48,21 @@ def main(cfg: DictConfig):
       layout="widescreen",
       scale=None)
 
+    feature_config = sv.FeatureConfig(skip=cfg.cols_definition.target_col)
+    compare_report = sv.compare(
+        [df_train, "Training Data"], [df_test, "Test Data"], feat_cfg=feature_config, pairwise_analysis="off")
+    file_compare_report = save_dir / "sweetviz_compare_report.html"
+    compare_report.show_html(
+      filepath=file_compare_report,
+      open_browser=False,
+      layout="widescreen",
+      scale=None)
+
     mlflow.set_tracking_uri("file://"+str(HOME_PATH / "experiments/mlruns"))
     mlflow.set_experiment(cfg.exp_name)
     with mlflow.start_run(run_name=cfg.run_name):
         mlflow.log_params(cfg)
-        mlflow.log_artifact(file_report)
+        mlflow.log_artifacts(save_dir)
         mlflow.log_artifacts(cwd_hydra)
 
 
